49c1b8a2e24ddf323bf98db8cb385d7512184e04
andrewhill <andrew.hill15@imperial.ac.uk>
2018-06-11 21:21:32 +0100

    version 1

---
+++ ifft-tube-status-notification.php
@@ -0,0 +1,37 @@
+<?php
+
+$lines = ["central", "jubilee", "london-overground"];
+
+$lines_argument = array_reduce($lines, function ($carry, $item) {
+    return $carry . $item . ",";
+});
+
+$json_response = file_get_contents("https://api.tfl.gov.uk/Line/$lines_argument/Status?detail=false");
+
+$tube_status = json_decode($json_response);
+
+$response = "";
+foreach ($tube_status as $line) {
+    $name = $line->name;
+    $status = $line->lineStatuses[0]->statusSeverityDescription;
+    if(property_exists($line->lineStatuses[0], 'reason')) {
+        $response .= $line->lineStatuses[0]->reason . "\n";
+    } else {
+        $response .= "$name: $status\n";
+    }
+}
+
+echo $response . "\n";
+
+$data = json_encode(array("value1" => $response));
+
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/json",
+        'method'  => 'POST',
+        'content' => $data
+    )
+);
+$context  = stream_context_create($options);
+$url = "https://maker.ifttt.com/trigger/tube_notify/with/key/vMdF3AjkPPzNrkudui8VX";
+echo file_get_contents($url, false, $context);
